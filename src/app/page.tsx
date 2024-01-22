ce52274d5b793d71f20200559b81bd295e6f93da
pratik-win-PC <pratiktravelogue@gmail.com>
2024-01-22 11:32:24 +0530

    intial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  return (
+    <div className="overflow-x-hidden text-white">
+      <Navbar/>
+      <Hero />
+    </div>
+  );
+}
