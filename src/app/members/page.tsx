5d4720be5084db176a2b4bf6df38adbff07e0f35
pratik_codes <66104012+pratik123-coder@users.noreply.github.com>
2024-02-26 09:42:31 +0530

    Added Dummy Members and Speakers Page

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,7 +1,19 @@
+import React from 'react';
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
 const Members = () => {
-  return ( 
-    <div>404 not found</div>
-   );
+  return (
+    <>
+      <Navbar />
+      <div className="h-screen flex flex-col items-center justify-center">
+        <div className="text-indigo-500 text-8xl font-semibold text-center">
+          Members
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
- 
+
 export default Members;
