d63168cc928c0237a6ec004f9dc931b4ff8b0831
pratik_codes <66104012+pratik123-coder@users.noreply.github.com>
2024-03-08 14:11:43 +0530

    Export CSV Names Updated

--- prisma/seed.js
+++ prisma/seed.js
@@ -9,19 +9,19 @@ async function exportFormDataToCSV() {
   try {
     console.log('Fetching data from form1...');
     const form1Data = await prisma.form1.findMany();
-    await exportToCSV(form1Data, "form1.csv");
+    await exportToCSV(form1Data, "crossteam_registerations.csv");
 
     console.log('Fetching data from form2...');
     const form2Data = await prisma.form2.findMany();
-    await exportToCSV(form2Data, "form2.csv");
+    await exportToCSV(form2Data, "institutionalteam_registerations.csv");
 
     console.log('Fetching data from form3...');
     const form3Data = await prisma.form3.findMany();
-    await exportToCSV(form3Data, "form3.csv");
+    await exportToCSV(form3Data, "independentAdj_registerations.csv");
 
     console.log('Fetching data from form4...');
     const form4Data = await prisma.form4.findMany();
-    await exportToCSV(form4Data, "form4.csv");
+    await exportToCSV(form4Data, "iasa.csv");
 
     console.log('Fetching data from contact...');
     const contactData = await prisma.contact.findMany();
